975adac94a66e217b73d54f8187fdb56d48eddbb
Alexey Sidorovitch <sidorovitch@gmail.com>
2016-10-12 13:21:48 +0300

    Album component

--- app/services/spotify.service.ts
+++ app/services/spotify.service.ts
@@ -7,6 +7,7 @@ export class SpotifyService {
     private searchUrl: string;
     private artistUrl: string;
     private albumsUrl: string;
+    private albumUrl: string;
 
     constructor(private _http:Http){
 
@@ -20,13 +21,18 @@ export class SpotifyService {
             .map(res => res.json());
     }
     getArtist(id:string){
-        this.artistUrl = 'https://api.spotify.com/v1/artists/'+id;
+        this.artistUrl = 'https://api.spotify.com/v1/artists/'+ id;
         return this._http.get(this.artistUrl)
             .map(res => res.json());
     }
     getAlbums(artistId:string){
-        this.albumsUrl = 'https://api.spotify.com/v1/artists/'+artistId + '/albums';
+        this.albumsUrl = 'https://api.spotify.com/v1/artists/'+ artistId + '/albums';
         return this._http.get(this.albumsUrl)
+            .map(res => res.json());
+    }
+    getAlbum(id:string){
+        this.albumUrl = 'https://api.spotify.com/v1/albums/' + id;
+        return this._http.get(this.albumUrl)
             .map(res => res.json());
     }
 }
